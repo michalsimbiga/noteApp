a7e84cf4e6ab2dff501aeb6f47762b2b6dcf926e
Simbiga Michal <MSimbiga@hemmersbach.com>
2020-01-15 17:27:32 +0100

    Add bottom nav

--- app/src/main/java/com/application/ui/overview/OverviewActivity.kt
+++ app/src/main/java/com/application/ui/overview/OverviewActivity.kt
@@ -1,8 +1,13 @@
 package com.application.ui.overview
 
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.application.R
 import com.application.ui.base.BaseActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_overview.*
 
 class OverviewActivity : BaseActivity() {
 
@@ -10,5 +15,13 @@ class OverviewActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_overview)
+        setUpNavigation()
+    }
+
+    private fun setUpNavigation() {
+//        NavigationUI.setupWithNavController(
+//            bottom_navigation,
+//            nav_host_fragment.findNavController()
+//        )
     }
 }
